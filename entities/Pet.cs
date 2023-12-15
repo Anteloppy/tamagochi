3426a530f33f4f15e1f30edbefa87c2a56c993fd
Anteloppy <polsonya04@gmail.com>
2023-12-15 12:41:53 +0700

    Добавьте файлы проекта.

---
+++ entities/Pet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamagochi.entities
+{
+    internal class Pet
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int type { get; set; }
+        public bool gender { get; set; }
+        public decimal age { get; set; }
+    }
+}
---
+++ entities/Stat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamagochi.entities
+{
+    internal class Stat
+    {
+        public int id { get; set; }
+        public decimal hunger { get; set; }
+        public decimal thirst { get; set; }
+        public decimal mood { get; set; }
+        public decimal sleepiness { get; set; }
+        public decimal beauty { get; set; }
+        public decimal health { get; set; }
+    }
+}
